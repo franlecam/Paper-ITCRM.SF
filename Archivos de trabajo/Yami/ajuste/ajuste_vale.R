dd1b5b9f339d1c1057e3ffc01bff61711421bd57
yamis1 <yamisonder@gmail.com>
2026-02-01 20:29:57 -0300

    ajuste

--- Archivos de trabajo/Yami/ajuste/ajuste_vale.R
+++ Archivos de trabajo/Yami/ajuste/ajuste_vale.R
@@ -4,7 +4,7 @@ library(dynlm)
 #Datos####
 datos <- read_excel("datos.xlsx",
                     sheet = "Tabelle2",
-                    range = "K2:S278")
+                    range = "K2:T278")
 
 attach(datos)
 #DESCRIPTIVAS ####
@@ -77,3 +77,106 @@ summary(reg_ARG_mult_fil_2)
 summary(reg_SFE_BCRA_mult_fil_2)
 summary(reg_SFE_Berre_mult_fil_2)
 
+## X SIN FILTRAR | API (AGRICULTURAL PRICE INDEX, THE PINK SHEET) ####
+reg_ARG_mult_3       <- lm(log(X) ~ log(ITCRM_ARG_BCRA) + log(API))
+reg_SFE_BCRA_mult_3  <- lm(log(X) ~ log(ITCRM_SFE_BCRA) + log(API))
+reg_SFE_Berre_mult_3 <- lm(log(X) ~ log(ITCRM_SFE_berre) + log(API))
+
+summary(reg_ARG_mult_3)
+summary(reg_SFE_BCRA_mult_3)
+summary(reg_SFE_Berre_mult_3)
+
+## X SIN FILTRAR | API + GEBC ####
+reg_ARG_mult_4       <- lm(log(X) ~ log(ITCRM_ARG_BCRA) + log(GEBC)  + log(API))
+reg_SFE_BCRA_mult_4  <- lm(log(X) ~ log(ITCRM_SFE_BCRA) + log(GEBC)  + log(API))
+reg_SFE_Berre_mult_4 <- lm(log(X) ~ log(ITCRM_SFE_berre) + log(GEBC) + log(API))
+
+summary(reg_ARG_mult_4)
+summary(reg_SFE_BCRA_mult_4)
+summary(reg_SFE_Berre_mult_4)
+
+install.packages(c("broom", "dplyr", "tidyr", "openxlsx"))
+library(broom)
+library(dplyr)
+library(tidyr)
+library(openxlsx)
+
+modelos <- list(
+  
+  # === MODELOS SIMPLES ===
+  "ARG | ITCRM ARG BCRA"        = reg_ARG,
+  "SFE | ITCRM SFE BCRA"        = reg_SFE_BCRA,
+  "SFE | ITCRM SFE Berre"       = reg_SFE_Berre,
+  
+  # === X FILTRADAS ===
+  "ARG FIL | ITCRM ARG BCRA"    = reg_ARG_fil,
+  "SFE FIL | ITCRM SFE BCRA"    = reg_SFE_BCRA_fil,
+  "SFE FIL | ITCRM SFE Berre"   = reg_SFE_Berre_fil,
+  
+  # === MULTIPLES | GEBC ===
+  "ARG | ITCRM ARG BCRA + GEBC"      = reg_ARG_mult,
+  "SFE | ITCRM SFE BCRA + GEBC"      = reg_SFE_BCRA_mult,
+  "SFE | ITCRM SFE Berre + GEBC"  = reg_SFE_Berre_mult,
+  
+  # === FILTRADAS | GEBC ===
+  "ARG FIL | ITCRM ARG BCRA + GEBC"  = reg_ARG_mult_fil,
+  "SFE FIL | ITCRM SFE BCRA + GEBC"  = reg_SFE_BCRA_mult_fil,
+  "SFE FIL | ITCRM SFE Berre + GEBC" = reg_SFE_Berre_mult_fil,
+  
+  # === GEBC + VMT ===
+  "ARG | ITCRM ARG BCRA + GEBC + VMT" = reg_ARG_mult_2,
+  "SFE | ITCRM SFE BCRA + GEBC + VMT" = reg_SFE_BCRA_mult_2,
+  "SFE | ITCRM SFE Berre + GEBC + VMT" = reg_SFE_Berre_mult_2,
+  
+  # === API ===
+  "ARG | ITCRM ARG BCRA + API"       = reg_ARG_mult_3,
+  "SFE | ITCRM SFE BCRA + API"       = reg_SFE_BCRA_mult_3,
+  "SFE | ITCRM SFE Berre + API"   = reg_SFE_Berre_mult_3,
+  
+  # === API + GEBC ===
+  "ARG | ITCRM ARG BCRA + API + GEBC" = reg_ARG_mult_4,
+  "SFE | ITCRM SFE BCRA + API + GEBC" = reg_SFE_BCRA_mult_4,
+  "SFE | ITCRM SFE Berre + API + GEBC" = reg_SFE_Berre_mult_4
+)
+
+coef_tabla <- bind_rows(
+  lapply(names(modelos), function(nm) {
+    tidy(modelos[[nm]]) %>%
+      mutate(
+        modelo = nm,
+        stars = case_when(
+          p.value < 0.01 ~ "***",
+          p.value < 0.05 ~ "**",
+          p.value < 0.10 ~ "*",
+          TRUE ~ ""
+        ),
+        coef = sprintf("%.3f%s", estimate, stars)
+      ) %>%
+      select(modelo, term, coef)
+  })
+)
+
+tabla_reg <- coef_tabla %>%
+  pivot_wider(
+    names_from  = modelo,
+    values_from = coef
+  )
+
+r2_adj <- tibble(
+  term = "R² ajustado",
+  !!!setNames(
+    lapply(modelos, function(m)
+      as.character(round(summary(m)$adj.r.squared, 3))
+    ),
+    names(modelos)
+  )
+)
+
+
+tabla_final <- bind_rows(tabla_reg, r2_adj)
+
+write.xlsx(
+  tabla_final,
+  file = "regresiones_completas.xlsx",
+  overwrite = TRUE
+)
